b20e900f1d9bb2da3785c4540457ad79a9dc36d6
podiujop <podebradyujop@yandex.ru>
2016-03-21 19:43:23 +0100

    Go

--- js/app.js
+++ js/app.js
@@ -1,33 +1,18 @@
 $(document).ready(function () {
 
-
 	$("#continue").click(function  () {
 		$("#under-maintance").css("display", "none");
 	});
-
-	function removeActive () {
-		$(".aLinks").data("data-active" , 0).attr("data-active" , 0);
-	};
 
 	//Kilograms to grams
 	$(".aLinks").click(function (event) {
 		event.preventDefault();
 		$(".aLinks").parent().attr("class", "non-active");
-
-		removeActive();
-		$(this).data("data-active" , 1).attr("data-active" , 1);
 		$(this).parent().addClass("active");
-
-		//for (i = 0; i <= $(".aLinks").length; i++) {
-			// var getActiveAttr = $(".aLinks").attr("data-active");
-			// console.log(getActiveAttr);
-			// if (getActiveAttr === 1) {
-			// 	$(this).parent().addClass("active")
-			// } else {
-			// 	$(".aLinks").removeClass("active");
-		//}
-
 	});
 
-
+	$("#kgToG").click(function () {$("#frameLoad").load("kgtogram.html");});
+	$("#kgToMg").click(function () {$("#frameLoad").load("kgtomilligrams.html");});
+	$("#kgToTon").click(function () {$("#frameLoad").load("kgtotons.html");});
+	$("#kgToN").click(function () {$("#frameLoad").load("kgtonewton.html");});
 });
